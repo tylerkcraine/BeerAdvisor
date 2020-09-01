9d1ad4887dd1df0a8b0affcf6ec05962dd5c7cc0
tyler <tylerkecraine@gmail.com>
2020-09-01 17:02:18 -0400

    Initial commit

---
+++ app/src/main/java/com/hfad/beeradviser/BeerExpert.java
@@ -0,0 +1,19 @@
+package com.hfad.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<>();
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Red Moose");
+        }
+        else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
---
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -0,0 +1,35 @@
+package com.hfad.beeradviser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer (View view) {
+        //what is going on
+        //get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Get recommendations for the BeerExpert class
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append("\n");
+        }
+        brands.setText(brandsFormatted);
+    }
+}
